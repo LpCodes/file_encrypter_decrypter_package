2486a9bcca087f73ad7d82ae8d2f4cd34b5c1ce3
LpCodes <lovelesh_p@zohomail.in>
2022-07-26 17:44:39 +0530

    0.0.2

--- dum.py
+++ dum.py
@@ -1,1 +1,7 @@
 from file_encrypter_decrypter import File_Encrypter,File_Decrypter,Folder_Encrypter,Folder_Decrypter
+
+
+File_Encrypter("./dummy.text")
+File_Decrypter("./dummy.text")
+Folder_Encrypter("/folderpath")
+Folder_Decrypter("/folderpath")
